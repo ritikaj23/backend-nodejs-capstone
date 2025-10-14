266805b83b5659e52b35c09ef82727b9fe7a4007
ritikaj23 <128594913+ritikaj23@users.noreply.github.com>
2025-10-14 17:57:19 +0530

    Update app.js

--- secondChance-backend/app.js
+++ secondChance-backend/app.js
@@ -31,7 +31,7 @@ app.use(express.static(path.join(__dirname, 'public')))
 // Use Routes
 app.use('/api/secondchance/items', secondChanceRoutes)
 app.use('/api/auth', authRoutes)
-app.use('/api/secondchance/search', searchRoutes)
+
 
 // Global Error Handler
 app.use((err, req, res) => {
