14f32938ae01eae38a3b3f660664cdd5e488bb09
ritikaj23 <128594913+ritikaj23@users.noreply.github.com>
2025-10-14 18:17:44 +0530

    Update Navbar.js

--- secondChance-frontend/src/components/Navbar/Navbar.js
+++ secondChance-frontend/src/components/Navbar/Navbar.js
@@ -52,26 +52,7 @@ export default function Navbar() {
             <li className="nav-item">
               <Link className="nav-link" to="/app/search">Search</Link>
             </li>
-            <ul className="navbar-nav ml-auto">
-            {isLoggedIn ? (
-                                    <>
-                                    <li className="nav-item"> <span className="nav-link" style={{color: "black", cursor:"pointer"}} onClick={profileSecton}>Welcome, {userName}</span> </li>
-                                    <li className="nav-item"><button className="nav-link login-btn" onClick={handleLogout}>Logout</button></li>
-                                    </>
-                                    )  : (
-                                    <>
-                                        <li className="nav-item">
-
-                                        <Link className="nav-link login-btn" to="/app/login">Login</Link>
-                                        </li>
-                                        <li className="nav-item">
-                                        <Link className="nav-link register-btn" to="/app/register">Register</Link>
-                                        </li>
-                                    </>
-                                )
-
-                                }
-            </ul>
+          
           </ul>
         </div>
       </nav>
