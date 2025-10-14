e6b470ad43a367954872c7f794221d014fd1555a
ritikaj23 <128594913+ritikaj23@users.noreply.github.com>
2025-10-14 17:59:32 +0530

    Update authRoutes.js

--- secondChance-backend/routes/authRoutes.js
+++ secondChance-backend/routes/authRoutes.js
@@ -15,44 +15,7 @@ const logger = pino();  // Create a Pino logger instance
 dotenv.config();
 const JWT_SECRET = process.env.JWT_SECRET;
 
-router.post('/register', async (req, res) => {
-    try {
-      //Connect to `secondChance` in MongoDB through `connectToDatabase` in `db.js`.
-      const db = await connectToDatabase();
-      const collection = db.collection("users");
-      const existingEmail = await collection.findOne({ email: req.body.email });
 
-        if (existingEmail) {
-            logger.error('Email id already exists');
-            return res.status(400).json({ error: 'Email id already exists' });
-        }
-
-        const salt = await bcryptjs.genSalt(10);
-        const hash = await bcryptjs.hash(req.body.password, salt);
-        const email=req.body.email;
-        console.log('email is',email);
-        const newUser = await collection.insertOne({
-            email: req.body.email,
-            firstName: req.body.firstName,
-            lastName: req.body.lastName,
-            password: hash,
-            createdAt: new Date(),
-        });
-
-        const payload = {
-            user: {
-                id: newUser.insertedId,
-            },
-        };
-
-        const authtoken = jwt.sign(payload, JWT_SECRET);
-        logger.info('User registered successfully');
-        res.json({ authtoken,email });
-    } catch (e) {
-        logger.error(e);
-        return res.status(500).send('Internal server error');
-    }
-});
 
 router.post('/login', async (req, res) => {
     console.log("\n\n Inside login")
