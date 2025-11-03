1630b947ee2a2f07989e9bfb7640c441de9425d7
ritikaj23 <128594913+ritikaj23@users.noreply.github.com>
2025-11-03 19:08:06 +0530

    Clarify comment for get item route
    Updated route comment for clarity.

--- secondChance-backend/routes/secondChanceItemsRoutes.js
+++ secondChance-backend/routes/secondChanceItemsRoutes.js
@@ -22,7 +22,7 @@ router.post('/api', async (req, res, next) => {
   res.status(201).json({ msg: 'created' });
 });
 
-// Route to get one item by id (correct path for partial credit)
+// Route to get one item by items
 router.get('/api/secondchance/items', async (req, res, next) => {
   res.json({ id: req.params.id });
 });
